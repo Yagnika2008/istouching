389ecc2d0f39339a45366f9b947273443dec896b
Yagnika2008 <70867156+Yagnika2008@users.noreply.github.com>
2021-03-06 07:06:04 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,25 @@
+function setup() {
+  createCanvas(800,400);
+ fixed= createSprite(400, 200, 50, 50);
+ moving=createSprite(400,200,80,30);
+ fixed.shapeColor="green"
+ moving.shapeColor="green"
+}
+
+function draw() {
+  background(255,255,255);  
+  moving.x=World.mouseX
+  moving.y=World.mouseY
+
+  if(moving.x-fixed.x<fixed.width/2+moving.width/2&&
+    fixed.x-moving.x<fixed.width/2+moving.width/2){
+    fixed.shapeColor="red"
+    moving.shapeColor="red"
+  }
+  else{
+    fixed.shapeColor="green"
+    moving.shapeColor="green"
+  }
+
+  drawSprites();
+}
